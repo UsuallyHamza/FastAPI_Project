8a98dbe5627ea7d9c1228120f0630c6dde01a3b9
Muhammad Hamza Arshad <156232242+UsuallyHamza@users.noreply.github.com>
2026-08-22 18:49:19 +0500

    Authentication - Registration and Login with JWT done
    Secured password hashing with argon2
    JWT generation and verification
    built registration and login endpoints on the backend
    /me endpoint for getting current user

--- main.py
+++ main.py
@@ -110,6 +110,26 @@ async def user_posts_page(
 
 
 
+## login and register template_routes
+@app.get("/login", include_in_schema=False)
+async def login_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "login.html",
+        {"title": "Login"},
+    )
+
+
+@app.get("/register", include_in_schema=False)
+async def register_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "register.html",
+        {"title": "Register"},
+    )
+
+
+
 
 
 
